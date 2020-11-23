5ff0e6e18fee711632d9861a44d6bd89c649ccfd
Kornpob Bhirombhakdi <kbhirombhakdi@altalune.local>
2020-11-23 14:32:16 -0500

    v.1.2.1

--- jwst_proposal_helper/prepare_KN_nebular_spc.py
+++ jwst_proposal_helper/prepare_KN_nebular_spc.py
@@ -32,7 +32,8 @@ def prepare_KN_nebular_spc(wavelength_angstrom,luminosity_per_angstrom,luminosit
     luminosity_distance_cm = luminosity_distance_mpc * 1e6 * 3.086e18
     flam = luminosity_per_angstrom / (4. * np.pi * np.power(luminosity_distance_cm,2))
     mjy = flam2millijansky(wavelength_angstrom,flam)
-    out = {'micron':wavelength_micron,'mjy':mjy}
+    m = np.argwhere(wavelength_micron > 0.).flatten()
+    out = {'micron':wavelength_micron[m],'mjy':mjy[m]}
     out = pd.DataFrame(out)
     savefolder,saveprefix = container.data['savefolder'],container.data['saveprefix']
     string = './{0}/{1}_KN_{2}Mpc.dat'.format(savefolder,saveprefix,int(luminosity_distance_mpc))
